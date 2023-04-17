e2dd267f8163e810c78b722496e6eb33900d9902
dma446 <dma446@nyu.edu>
2023-04-17 04:29:29 -0400

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
@@ -33,18 +33,18 @@ const theme = createTheme({
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <ThemeProvider theme={theme}>
         <Navbar />
           <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/about' element={<About />} />
-            <Route exact path='/projects' element={<Projects />} />
-            <Route exact path='/contact' element={<Contact />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
           </Routes>
           <Footer />
       </ThemeProvider>
-    </Router>
+    </HashRouter>
   );
 }
 
